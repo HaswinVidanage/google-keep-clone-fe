503865a16e7c01761473c5bbb7b61c97217b12f1
Haswin <hashdevan@gmail.com>
2020-12-20 15:26:01 +0800

    feature/setup-ci-cd:  * setup ci cd

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,8 @@ deploy() {
     curl https://cli-assets.heroku.com/install-ubuntu.sh | sh
     heroku auth:token
     heroku container:login
-    heroku container:push react-webapp --app ${HEROKU_APP_NAME}
-    heroku container:release react-webapp --app ${HEROKU_APP_NAME}
+    heroku container:push web --app ${HEROKU_APP_NAME}
+    heroku container:release web --app ${HEROKU_APP_NAME}
     heroku logs --app ${HEROKU_APP_NAME}
 }
 
