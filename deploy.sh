f05b933032dff2390771baace4637ce41a0044ed
Haswin <hashdevan@gmail.com>
2020-12-20 11:36:25 +0800

    feature/setup-ci-cd: * dockerize app

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -e
+
+deploy() {
+    curl https://cli-assets.heroku.com/install-ubuntu.sh | sh
+    heroku auth:token
+    heroku container:login
+    heroku container:push --recursive --app ${HEROKU_APP_NAME}
+    heroku container:release web --app ${HEROKU_APP_NAME}
+    heroku logs --app ${HEROKU_APP_NAME}
+}
+
+if [[ $1 = 'deploy' ]]; then
+    deploy
+else
+    echo "Usage: ./deploy.sh (deploy)"
+    exit 2
+fi
